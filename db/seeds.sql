948812698e6fb398b5bcd6d277242807b2483093
Caleb Rose <clrose86@gmail.com>
2018-03-18 11:14:18 -0500

    seeds file filled

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Royal with Cheese");
+INSERT INTO burgers (burger_name) VALUES ("McDouble");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Bonanza");
+INSERT INTO burgers (burger_name) VALUES ("Buffalo and Blue Cheese");
