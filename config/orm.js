1247e9039b88722eebddc73fcbacb3132c66be7b
Caleb Rose <clrose86@gmail.com>
2018-03-18 12:39:30 -0500

    constructed orm

---
+++ config/orm.js
@@ -0,0 +1,31 @@
+var connection = require("./connection.js");
+
+var orm = {
+	selectAll: function(table, cb) {
+		var queryString = "SELECT * FROM " + table;
+		connection.query(queryString, function(err,res){
+			if (err) throw err;
+			cb(res);
+		});
+	},
+	insertOne: function(table, col, val, cb) {
+		var queryString = "INSERT INTO " + table;
+		queryString += " (" + col + ") ";
+		queryString += "VALUES (" + val + ") ;"; 
+		connection.query(queryString, function(err,res){
+			if(err) throw err;
+			cb(res);
+		});
+	},
+	updateOne: function(table, colVal, condition, cb){
+		var queryString = "UPDATE " + table;
+		queryString += "SET " + colVal;
+		queryString += " WHERE id=" + condition ";"; 
+		connection.query(queryString, function(err,res){
+			if(err) throw err;
+			cb(res);
+		});
+	}
+};
+
+module.exports = orm;
