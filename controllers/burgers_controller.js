ae8c29b2ad27b43716cea1618b92f34d145f26e7
Caleb Rose <clrose86@gmail.com>
2018-03-18 13:13:03 -0500

    made router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var burgerMaster = require("../models/burger.js");
+
+router.get("/", function(req, res){
+	res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res){
+	burgerMaster.getAll(function(burgerInfo) {
+		res.render("index", {burgerInfo: burgerInfo});
+	});
+});
+
+router.post("/burgers/create", function(req, res){
+	burgerMaster.makeOne(req.body.burger_name, function(newBurger){
+		console.log(newBurger);
+		res.redirect("/");
+	});
+});
+
+router.put("/burgers/:id", function(req,res){
+	burgerMaster.eatOne(req.params.id, function(eaten){
+		console.log(eaten);
+		res.sendStatus(200);
+	});
+});
+
+module.exports = router;
