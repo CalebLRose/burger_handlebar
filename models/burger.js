a334b8680772725d2920b7eaac9f22c0a25a18d7
Caleb Rose <clrose86@gmail.com>
2018-03-23 18:50:28 -0500

    oops nevermind

--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,15 @@ var orm = require("../config/orm.js");
 
 var burgerMaster = {
 	getAll: function(cb){
-		orm.selectAll("heroku_cebe177fc141cc9", function(res){
+		orm.selectAll("burgers", function(res){
 			cb(res);
 		});
 	},
 	makeOne: function(name, cb){
-		orm.insertOne("heroku_cebe177fc141cc9", "burger_name", name, cb);
+		orm.insertOne("burgers", "burger_name", name, cb);
 	},
 	eatOne: function(id, con, cb){
-		orm.updateOne("heroku_cebe177fc141cc9", id, con, cb);
+		orm.updateOne("burgers", id, con, cb);
 	}
 };
 
